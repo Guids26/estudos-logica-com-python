8200cf1efc026df52d73084028ebe9f6a0af8fbf
Guilherme Dantas <54046453+Guids26@users.noreply.github.com>
2024-10-08 14:58:09 +0000

    feat: add solving of ex01.py

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,7 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+minha_string = "Olá, este é um exemplo de string."
+
+minha_lista_de_strings = ["Primeira string", "Segunda string", "Terceira string", "Quarta string"]
+
+print(minha_string)
+print(minha_lista_de_strings)
